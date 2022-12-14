204ba69a5727b125d75005adce62a3b374938d9a
Jayesh7046 <tahilianij11@gmail.com>
2022-12-14 20:50:38 +0530

    13-12-2022

--- index.php
+++ index.php
@@ -7,33 +7,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Food Zone</title>
-    <link rel="stylesheet" href="./components/style2.css">
-    <!--custom css-->
+    <!-- <link rel="stylesheet" href="./design_files/style2.css"> -->
     <script src="https://cdn.tailwindcss.com"></script>
-    <!--tailwind css-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&family=Quicksand:wght@500&display=swap"
         rel="stylesheet">
 </head>
 
-<body>
-    <!-- mainbg -->
-    <div class="flex justify-center items-center h-screen w-screen bg-red-200">
-        <!-- secondbg -->
-        <div class="bg-white secondbg">
-            <!-- content div -->
-            <div class="mx-80 h-full w-4/6 thirdbg">
-                <!-- navbar -->
-                <nav class="content-center h-16 w-full bg-red-200">
-                    <div class="mt-5">
-                        <div class="space-x-5 ml-20">
-                            <?php
-                                nav_link(["Home","About","Special dishes"]);
-                            ?>
-                        </div>
-                    </div>
-                </nav>
+<body class="bg-violet-300">
+    <div class="bg-white h-screen m-5">
+        <div class="bg-red-500 mx-20">
+            <div class="flex justify-between">
+                <div><img src="https://icons8.com/icon/kSrfC0FPRg1e/hamburger-menu" alt=""></div>
+                <div>02</div>
+                <div>03</div>
             </div>
         </div>
     </div>
