266958ed2e3e47c8ef972933fc024b27afcd1927
Jayesh7046 <tahilianij11@gmail.com>
2022-12-19 21:27:51 +0530

    19

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
 <body class="bg-violet-300">
     <!-- Main Background -->
     <div class="flex justify-center bg-white h-screen m-5">
-        <div class="bg-blue-500 w-10/12 h-screen">
+        <div class="container bg-blue-500 w-10/12 h-screen">
             <!-- Navbar Start-->
-            <div class="bg-red-500 pt-6 mb-16">
+            <div class="bg-red-500 pt-6">
                 <div class="flex justify-between">
                     <div class="place-self-center">
                         <img src="./images/menu icon.png" alt="" class="img">
@@ -42,7 +42,7 @@
                 </div>
             </div>
             <!-- Navbar End -->
-            <div class="box p-2 m-4 -mt-4 bg-white">
+            <div class="box p-2 bg-white">
             <div class="container mx-auto  m-8 flex justify-between ">
             <div class="basis-1/2 "><h1 class="mt-36 space-y-2 bg-green-100 text-5xl cursive">We Love <br> Delicious Foods!</h1>
             <div class="grid grid-raw-3 gap-4 place-items-start ... space-y-2 mt-20">
@@ -51,17 +51,26 @@
                     <button class="ml-10"><a href="#" class="hover:text-red-600"><img src="./images/video.png" alt=""></button>
                     <div class="mt-2 ml-2 font-medium">Play Video</div></a>
                 </div>
-                <div>
-                    
+                    <div class="flex justify-between space-x-8 ">
+                        
+                        <div class="items-center">
+                            <span class="box-icon bg-white"><i class="fa-solid fa-rocket fa-2x"></i><br>Fast Delivery</span>
+                        </div>
+                        <div class="self-center">
+                            <span class="box-icon bg-white"><i class="snap-center fa-solid fa-leaf fa-2x"></i><br>Fresh Food</span>
+                        </div>
+                        <div class="place-center">
+                            <span class="box-icon bg-white "><i class="fa-solid fa-comment fa-2x"></i><br>24/7 Support</span>
+                        </div>
                 </div>
             </div>
             
             <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
             <!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
             </div>
-            <div class="basis-1/2 bg-blue-50 bg_food bg-no-repeat	bg-cover bg-center  flex justify-end items-end">
-                <span class="box-icon"><i class="fa-solid fa-angle-left fa-2x  text-blue-600"></i></span>
-                <span class="box-icon"><i class="fa-solid fa-angle-right fa-2x"></i></span>
+            <div class="basis-1/2 bg-blue-50 bg_food bg-no-repeat bg-cover bg-center  flex justify-end items-end">
+                <span class="box-icon bg-white"><i class="fa-solid fa-angle-left fa-2x  text-blue-600"></i></span>
+                <span class="box-icon bg-white"><i class="fa-solid fa-angle-right fa-2x"></i></span>
             </div>
         </div>
     </div>
