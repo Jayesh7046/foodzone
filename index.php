fe3fed59c84dcfa3eb833877af10a5a22159df15
Jayesh7046 <tahilianij11@gmail.com>
2022-12-22 19:56:07 +0530

    22

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&family=Quicksand:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&family=Quicksand:wght@500&display=swap"
+        rel="stylesheet">
 </head>
 
 <body class="bg-violet-300">
@@ -23,59 +24,63 @@
             <div class="bg-red-500 pt-6">
                 <div class="flex justify-between">
                     <div class="place-self-center">
-                        <img src="./images/menu icon.png" alt="" class="img">
+                        <i class="fa-solid fa-bars fa-2x"></i>
                     </div>
                     <div class="place-self-center space-x-4">
                         <?php 
-                        nav_link(["Home","About","Special Dishes"])
-                    ?>
+                            nav_link(["Home","About","Special Dishes"])
+                        ?>
                     </div>
                     <div class="place-self-center space-x-4 cursive">Resto</div>
                     <div class="place-self-center space-x-4">
                         <?php 
-                        nav_link(["Menu","Team","Reservation"])
-                    ?>
+                            nav_link(["Menu","Team","Reservation"])
+                        ?>
                     </div>
                     <div class="place-self-center space-x-4">
-                        <img src="./images/search.png" alt="" class="img">
+                        <i class="fa-solid fa-magnifying-glass fa-2x"></i>
                     </div>
                 </div>
             </div>
             <!-- Navbar End -->
+            <!-- First page start -->
             <div class="box p-2 bg-white">
-            <div class="container mx-auto  m-8 flex justify-between ">
-            <div class="basis-1/2 "><h1 class="mt-36 space-y-2 bg-green-100 text-5xl cursive">We Love <br> Delicious Foods!</h1>
-            <div class="grid grid-raw-3 gap-4 place-items-start ... space-y-2 mt-20">
-                <div class="flex justify-between">
-                    <button class="bg-red-600 rounded-full h-10 w-40 text-white font-medium">Exploer Menu</button>
-                    <button class="ml-10"><a href="#" class="hover:text-red-600"><img src="./images/video.png" alt=""></button>
-                    <div class="mt-2 ml-2 font-medium">Play Video</div></a>
+                <div class="container mx-auto  m-8 flex justify-between ">
+                    <div class="basis-1/2 ">
+                        <h1 class="mt-36 space-y-2 bg-green-100 text-5xl cursive">We Love <br> Delicious Foods!</h1>
+                        <div class="grid grid-raw-3 gap-4 place-items-start ... space-y-2 mt-20">
+                            <div class="flex justify-between mb-40">
+                                <button class="bg-red-600 rounded-full h-10 w-40 text-white font-medium">Exploer Menu</button>
+                                <button class="ml-10"><a href="#" class="hover:text-red-600"><i class="fa-solid fa-film fa-2x"></i></button>
+                                <div class="mt-2 ml-2 font-medium">Play Video</div></a>
+                            </div>
+                            <div class="flex justify-between">
+                                <i class="fa-solid fa-rocket fa-2x text-center pr-4 border-r-2 border-black">
+                                    <h1 class="text-base font-sans">Fast Delivery</h1>
+                                </i>
+                                <i class="fa-solid fa-leaf fa-2x text-center pr-4 mx-4 border-r-2 border-black">
+                                    <h1 class="text-base font-sans">Fresh Food</h1>
+                                </i>
+                                <i class="fa-solid fa-comment fa-2x text-center">
+                                    <h1 class="text-base font-sans">24/7 Support</h1>
+                                </i>
+                            </div>
+                        </div>
+
+                        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                        <!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
+                    </div>
+                    <div
+                        class="basis-1/2 bg-blue-50 bg_food bg-no-repeat bg-cover bg-center  flex justify-end items-end">
+                        <span class="box-icon bg-white"><i class="fa-solid fa-angle-left fa-2x  text-blue-600"></i></span>
+                        <span class="box-icon bg-white"><i class="fa-solid fa-angle-right fa-2x"></i></span>
+                    </div>
                 </div>
-                    <div class="flex justify-between space-x-8 ">
-                        
-                        <div class="items-center">
-                            <span class="box-icon bg-white"><i class="fa-solid fa-rocket fa-2x"></i><br>Fast Delivery</span>
-                        </div>
-                        <div class="self-center">
-                            <span class="box-icon bg-white"><i class="snap-center fa-solid fa-leaf fa-2x"></i><br>Fresh Food</span>
-                        </div>
-                        <div class="place-center">
-                            <span class="box-icon bg-white "><i class="fa-solid fa-comment fa-2x"></i><br>24/7 Support</span>
-                        </div>
-                </div>
             </div>
-            
-            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-            <!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
-            </div>
-            <div class="basis-1/2 bg-blue-50 bg_food bg-no-repeat bg-cover bg-center  flex justify-end items-end">
-                <span class="box-icon bg-white"><i class="fa-solid fa-angle-left fa-2x  text-blue-600"></i></span>
-                <span class="box-icon bg-white"><i class="fa-solid fa-angle-right fa-2x"></i></span>
-            </div>
-        </div>
-    </div>
-        
-        </div>
+            <!-- end first page -->
+            <!-- Second page -->
+            <div class=""></div>
+
         </div>
     </div>
 </body>
